1a6762a9a3a9441a9a43a38a18911731b95b157a
Eyri Cho <eyricho@live.com>
2020-08-24 14:59:50 +0800

    Learned Lesson 3.

--- main.go
+++ main.go
@@ -2,8 +2,35 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello world.")
+	// lesson 3
+	var command = "walk outside"
+	var exit = strings.Contains(command, "outside")
+	fmt.Println("You leave the cave:", exit)
+	if command == "walk outside" {
+		fmt.Println("You head further up the mountain")
+	} else if command == "walk inside" {
+		fmt.Println("You head further deep in the cavern")
+	} else {
+		fmt.Println("You have completely losed")
+	}
+	var room = "lake"
+	switch room {
+	case "cave":
+		fmt.Println("You find yourself in a dimly lit cavern")
+	case "lake":
+		fmt.Println("The ice seems solid enough")
+		fallthrough
+	case "underwater":
+		fmt.Println("The water is freezing cold")
+	}
+
+	var num = 10
+	for num > 0 {
+		fmt.Printf("%-4v", num)
+		num--
+	}
 }
