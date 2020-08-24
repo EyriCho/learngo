52df2377d1865958ac50b3fc913c4a26bab3daec
Eyri Cho <eyricho@live.com>
2020-08-24 15:12:53 +0800

    Learned Lesson 4

--- main.go
+++ main.go
@@ -2,8 +2,30 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
+var era = "AD"
 func main() {
-	fmt.Println("Hello world.")
+	// lesson 4
+	count := 10
+	for count > 0 {
+		month := rand.Intn(12) + 1
+		year := rand.Intn(9999)
+		daysInMonth := 31
+		switch month {
+			case 2:
+				if year % 400 == 0 || (year % 100 != 0 && year % 4 == 0) {
+					daysInMonth = 29
+				} else {
+					daysInMonth = 28
+				}
+			case 4, 6, 9, 11:
+			daysInMonth = 30
+		}
+		day := rand.Intn(daysInMonth) + 1
+		fmt.Println(era, year, month, day)
+
+		count--
+	}
 }
