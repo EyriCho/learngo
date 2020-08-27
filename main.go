72e23e10fde828c68e0291e1d8f42ead4f62ad23
Eyri Cho <eyricho@live.com>
2020-08-27 11:30:57 +0800

    Learned Lesson 8

--- main.go
+++ main.go
@@ -2,8 +2,30 @@ package main
 
 import (
 	"fmt"
+	"math/big"
 )
 
 func main() {
-	fmt.Println("Hello world.")
+	// lesson 8
+
+	// big package
+	lightSpeed := big.NewInt(299792)
+	secondsPerDay := big.NewInt(86400)
+
+	distance := new(big.Int)
+	distance.SetString("24000000000000000000", 10)
+	fmt.Println("Andromeda Galaxy is", distance, "km away.")
+	seconds := new(big.Int)
+	seconds.Div(distance, lightSpeed)
+
+	days := new(big.Int)
+	days.Div(seconds, secondsPerDay)
+
+	fmt.Println("That is", days, "days of travel at light speed")
+
+	// constant
+	const lightSpeedConst = 299792
+	const secondsPerDayConst = 86400
+	const daysPerYearConst = 365
+	fmt.Println("Canis Major Dwarf is", 236000000000000000 / lightSpeedConst / secondsPerDayConst / daysPerYearConst, "light years away.")
 }
